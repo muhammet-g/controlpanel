4d92f121bfa2594bf97577cbb7c8eb3697b13779
muhammet-g <muhammetga642@gmail.com>
2024-07-15 19:19:34 +0300

    2 commit

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,27 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 module.exports = {
   entry: {
     app: "./src/index.js",
+    "assets/js/banner": "./src/assets/js/banner.js",
+    // "assets/js/chart": "./src/assets/js/chart.js",
+    // "assets/js/tabs": "./src/assets/js/tabs.js",
+    // "assets/js/upload": "./src/assets/js/upload.js",
   },
   output: {
     path: path.join(__dirname, "/app"),
-    filename: "app.js",
+    filename: "[name].js",
   },
   module: {
     rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
       {
         test: /\.html$/,
         use: [
@@ -32,10 +46,11 @@ module.exports = {
         ],
       },
       {
-        test: /\.(woff(2)?|ttf|eot)$/,
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        exclude: /images/,
         type: "asset/resource",
         generator: {
-          filename: "assets/fonts/[name][ext]",
+          filename: "assets/[name].[ext]",
         },
       },
     ],
@@ -56,9 +71,31 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "assets/css/style.css",
     }),
+
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "./src/index.html",
+      chunks: ["app"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "components/button.html",
+      template: "./src/components/button.html",
+      chunks: ["app"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "components/textfield.html",
+      template: "./src/components/textfield.html",
+      chunks: ["app"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "components/card.html",
+      template: "./src/components/card.html",
+      chunks: ["app"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "components/banner.html",
+      template: "./src/components/banner.html",
+      chunks: ["app", "assets/js/banner"],
     }),
   ],
   optimization: {
@@ -66,98 +103,3 @@ module.exports = {
     minimizer: [`...`, new CssMinimizerPlugin()],
   },
 };
-
-// const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-// // const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-// module.exports = {
-//   entry: {
-//     app: "./src/index.js",
-//   },
-//   output: {
-//     path: path.join(__dirname, "/app"),
-//     filename: "app.js",
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.html$/,
-//         use: [
-//           {
-//             loader: "html-loader",
-//           },
-//         ],
-//       },
-//       {
-//         test: /\.(sass|css|scss)$/,
-
-//         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-//       },
-//       {
-//         test: /\.scss$/,
-//         use: [
-//           MiniCssExtractPlugin.loader,
-//           "css-loader",
-//           "postcss-loader",
-//           "sass-loader",
-//         ],
-//       },
-//       {
-//         test: /\.css$/,
-//         use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
-//       },
-//       {
-//         test: /\.(svg|eot|woff|woff2|ttf)$/,
-
-//         exclude: /images/,
-
-//         use: [
-//           {
-//             loader: "file-loader",
-
-//             options: {
-//               name: "[name].[ext]",
-
-//               outputPath: "assets/fonts",
-//             },
-//           },
-//         ],
-//       },
-//     ],
-//   },
-//   devServer: {
-//     static: {
-//       directory: path.join(__dirname, "app"),
-//     },
-//     compress: true,
-//     port: 8081,
-//     open: true,
-//     devMiddleware: {
-//       writeToDisk: true,
-//     },
-//   },
-//   plugins: [
-//     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-//     // new OptimizeCSSAssetsPlugin({}),
-//     new MiniCssExtractPlugin({
-//       filename: "assets/css/style.css",
-//     }),
-//     new HtmlWebpackPlugin({
-//       filename: "index.html",
-//       template: "./src/index.html",
-//     }),
-//     new MiniCssExtractPlugin({
-//       filename: "[name].css",
-//       chunkFilename: "[id].css",
-//     }),
-//   ],
-//   optimization: {
-//     minimizer: [
-//       `...`, // Include the default minimizers (i.e., Terser for JS)
-//       new CssMinimizerPlugin(),
-//     ],
-//   },
-// };
